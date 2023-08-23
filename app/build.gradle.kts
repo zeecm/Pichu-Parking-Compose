177a5d952cce45593e32106c47f07e8ff0814981
Brighten <pichubabe1@gmail.com>
2023-08-23 21:14:08 +0800

    added permissions request

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,7 +55,7 @@ android {
 }
 
 dependencies {
-
+    implementation ("com.google.accompanist:accompanist-permissions:0.21.1-beta")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.0")
--- app/src/main/java/com/pichugroup/pichuparking/MainActivity.kt
+++ app/src/main/java/com/pichugroup/pichuparking/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.pichugroup.pichuparking
 
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.Manifest
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
@@ -19,6 +27,35 @@ import com.google.maps.android.compose.rememberCameraPositionState
 import com.pichugroup.pichuparking.ui.theme.PichuParkingComposeTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                setContent {
+                    PichuParkingComposeTheme {
+                        Surface(
+                            modifier = Modifier.fillMaxSize(),
+                            color = MaterialTheme.colorScheme.background
+                        ) {
+                            ShowGoogleMaps()
+                        }
+                    }
+                }
+            } else {
+                setContent{
+                    PichuParkingComposeTheme {
+                        Surface(
+                            modifier = Modifier.fillMaxSize(),
+                            color = MaterialTheme.colorScheme.background
+                        ) {
+                            LocationPermissionMessage()
+                        }
+                    }
+                }
+
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -28,31 +65,47 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    showGoogleMaps()
+                    if (hasLocationPermission()) {
+                        ShowGoogleMaps()
+                    } else {
+                        requestLocationPermission()
+                    }
                 }
             }
         }
     }
+
+    private fun hasLocationPermission(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestLocationPermission() {
+        if (!hasLocationPermission()) {
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
+
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun LocationPermissionMessage() {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PichuParkingComposeTheme {
-        Greeting("Zhao Cong")
+    ) {
+        Text(
+            text = "Location permissions are required to use the app.",
+            modifier = Modifier.padding(16.dp)
+        )
     }
 }
-
 @Composable
-fun showGoogleMaps() {
+fun ShowGoogleMaps() {
     val singapore = LatLng(1.35, 103.87)
     val cameraPositionState = rememberCameraPositionState {
         position = CameraPosition.fromLatLngZoom(singapore, 10f)
@@ -73,6 +126,6 @@ fun showGoogleMaps() {
 @Composable
 fun MapsPreview() {
     PichuParkingComposeTheme {
-        showGoogleMaps()
+        ShowGoogleMaps()
     }
 }
