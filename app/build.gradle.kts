30fddfc2c61ee1d1dfda86578a32eec86dbab71e
zhaocong <zhaocong.mah@gmail.com>
2023-08-24 22:39:31 +0800

    Fix dialog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,16 +55,17 @@ android {
 }
 
 dependencies {
-    implementation ("com.google.accompanist:accompanist-permissions:0.21.1-beta")
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+    implementation("androidx.activity:activity-compose:1.7.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("com.google.maps.android:maps-compose:2.13.0")
     implementation("com.google.android.gms:play-services-maps:18.1.0")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
     implementation("androidx.compose.material3:material3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- app/src/main/java/com/pichugroup/pichuparking/MainActivity.kt
+++ app/src/main/java/com/pichugroup/pichuparking/MainActivity.kt
@@ -1,23 +1,27 @@
 package com.pichugroup.pichuparking
 
+import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.Manifest
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Card
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.ViewModel
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
@@ -25,55 +29,38 @@ import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.pichugroup.pichuparking.ui.theme.PichuParkingComposeTheme
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class MainActivity : ComponentActivity() {
-
+    private val locationPermissionWarningViewModel: LocationPermissionWarningViewModel by viewModels()
     private val requestPermissionLauncher =
         registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
-            if (isGranted) {
-                setContent {
-                    PichuParkingComposeTheme {
-                        Surface(
-                            modifier = Modifier.fillMaxSize(),
-                            color = MaterialTheme.colorScheme.background
-                        ) {
-                            ShowGoogleMaps()
-                        }
-                    }
-                }
-            } else {
-                setContent{
-                    PichuParkingComposeTheme {
-                        Surface(
-                            modifier = Modifier.fillMaxSize(),
-                            color = MaterialTheme.colorScheme.background
-                        ) {
-                            LocationPermissionMessage()
-                        }
-                    }
-                }
-
+            if (!isGranted) {
+                locationPermissionWarningViewModel.triggerDialog()
             }
         }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val showWarning: Boolean by locationPermissionWarningViewModel.dialogActive.collectAsState()
             PichuParkingComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    if (hasLocationPermission()) {
-                        ShowGoogleMaps()
-                    } else {
-                        requestLocationPermission()
+                Column {
+                    DisplayGoogleMaps()
+                    if (showWarning) {
+                        LocationPermissionWarning(
+                            active = true,
+                            onAccept = locationPermissionWarningViewModel::onDialogAccept,
+                        )
                     }
                 }
             }
         }
+        requestLocationPermission()
     }
+
 
     private fun hasLocationPermission(): Boolean {
         return ContextCompat.checkSelfPermission(
@@ -88,24 +75,43 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+
 }
 
 @Composable
-fun LocationPermissionMessage() {
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp)
-
-    ) {
-        Text(
-            text = "Location permissions are required to use the app.",
-            modifier = Modifier.padding(16.dp)
-        )
+fun LocationPermissionWarning(active: Boolean, onAccept: () -> Unit) {
+    if (active) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Black.copy(alpha = 0.6f))
+        ) {
+            AlertDialog(
+                onDismissRequest = onAccept,
+                title = { Text("Location required for some features") },
+                text = { Text("Location permissions are required for features that show your current location on the map") },
+                confirmButton = {
+                    TextButton(onClick = onAccept) {
+                        Text("I know stfu".uppercase())
+                    }
+                },
+            )
+        }
     }
 }
+
+
+@Preview(showSystemUi = true, showBackground = true)
 @Composable
-fun ShowGoogleMaps() {
+fun CardsPreview() {
+    fun doesNothing() {}
+    PichuParkingComposeTheme {
+        LocationPermissionWarning(active = true, onAccept = ::doesNothing)
+    }
+}
+
+@Composable
+fun DisplayGoogleMaps() {
     val singapore = LatLng(1.35, 103.87)
     val cameraPositionState = rememberCameraPositionState {
         position = CameraPosition.fromLatLngZoom(singapore, 10f)
@@ -113,19 +119,18 @@ fun ShowGoogleMaps() {
     GoogleMap(
         modifier = Modifier.fillMaxSize(),
         cameraPositionState = cameraPositionState
-    ) {
-        Marker(
-            state = MarkerState(position = singapore),
-            title = "Singapore",
-            snippet = "Marker in Singapore"
-        )
-    }
+    )
 }
 
-@Preview(showBackground = true, showSystemUi = true)
-@Composable
-fun MapsPreview() {
-    PichuParkingComposeTheme {
-        ShowGoogleMaps()
+class LocationPermissionWarningViewModel : ViewModel() {
+    private val _dialogActive = MutableStateFlow(false)
+    val dialogActive: StateFlow<Boolean> = _dialogActive.asStateFlow()
+
+    fun triggerDialog() {
+        _dialogActive.value = true
+    }
+
+    fun onDialogAccept() {
+        _dialogActive.value = false
     }
 }
