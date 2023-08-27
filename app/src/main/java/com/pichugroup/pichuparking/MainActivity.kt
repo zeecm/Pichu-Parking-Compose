4e06cea7991bf995187d05d5eb3d9ce3adbad12e
zeecmah <114866293+zeecmah@users.noreply.github.com>
2023-08-27 16:12:10 +0800

    Refactor and use experimental permissions

--- app/src/main/java/com/pichugroup/pichuparking/MainActivity.kt
+++ app/src/main/java/com/pichugroup/pichuparking/MainActivity.kt
@@ -1,171 +1,21 @@
 package com.pichugroup.pichuparking
 
-import android.Manifest
-import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.activity.viewModels
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.LocationOn
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.core.content.ContextCompat
-import androidx.lifecycle.ViewModel
-import com.google.android.gms.maps.model.CameraPosition
-import com.google.android.gms.maps.model.LatLng
-import com.google.maps.android.compose.GoogleMap
-import com.google.maps.android.compose.MapUiSettings
-import com.google.maps.android.compose.rememberCameraPositionState
+import com.pichugroup.pichuparking.screens.GoogleMapViewScreen
 import com.pichugroup.pichuparking.ui.theme.PichuParkingComposeTheme
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
 
 class MainActivity : ComponentActivity() {
-    private val locationPermissionWarningViewModel: LocationPermissionWarningViewModel by viewModels()
-    private val requestPermissionLauncher =
-        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
-            if (!isGranted) {
-                locationPermissionWarningViewModel.triggerDialog()
-            }
-        }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val showWarning: Boolean by locationPermissionWarningViewModel.dialogActive.collectAsState()
             PichuParkingComposeTheme {
                 Column {
-                    GoogleMapView()
-                    if (showWarning) {
-                        LocationPermissionWarning(
-                            active = true,
-                            onAccept = locationPermissionWarningViewModel::onDialogAccept,
-                        )
-                    }
+                    GoogleMapViewScreen()
                 }
             }
         }
-        requestLocationPermission()
-    }
-
-
-    private fun hasLocationPermission(): Boolean {
-        return ContextCompat.checkSelfPermission(
-            this,
-            Manifest.permission.ACCESS_FINE_LOCATION
-        ) == PackageManager.PERMISSION_GRANTED
-    }
-
-    private fun requestLocationPermission() {
-        if (!hasLocationPermission()) {
-            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
-        }
-    }
-
-
-}
-
-@Composable
-fun LocationPermissionWarning(active: Boolean, onAccept: () -> Unit) {
-    if (active) {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Color.Black.copy(alpha = 0.6f))
-        ) {
-            AlertDialog(
-                onDismissRequest = onAccept,
-                title = { Text("Location required for some features") },
-                text = { Text("Location permissions are required for features that show your current location on the map") },
-                confirmButton = {
-                    TextButton(onClick = onAccept) {
-                        Text("I know stfu".uppercase())
-                    }
-                },
-            )
-        }
-    }
-}
-
-
-@Preview(showSystemUi = true, showBackground = true)
-@Composable
-fun CardsPreview() {
-    fun doesNothing() {}
-    PichuParkingComposeTheme {
-        LocationPermissionWarning(active = true, onAccept = ::doesNothing)
-    }
-}
-
-@Composable
-fun GoogleMapView() {Box {
-        DisplayGoogleMaps()
-        FloatingActionButton(
-            onClick = { /*TODO*/ },
-            modifier = Modifier
-                .padding(16.dp)
-                .align(Alignment.BottomEnd)
-                .clip(CircleShape)
-        ) {
-            Icon(imageVector = Icons.Default.LocationOn, contentDescription = "Current Location")
-        }
-    }
-}
-
-@Composable
-fun DisplayGoogleMaps() {
-    val singapore = LatLng(1.35, 103.87)
-    val cameraPositionState = rememberCameraPositionState {
-        position = CameraPosition.fromLatLngZoom(singapore, 10f)
-    }
-    var uiSettings by remember { mutableStateOf(MapUiSettings(zoomControlsEnabled = false))}
-    GoogleMap(
-        modifier = Modifier.fillMaxSize(),
-        cameraPositionState = cameraPositionState,
-        uiSettings = uiSettings,
-    )
-}
-
-
-@Preview(showBackground = true, showSystemUi = true)
-@Composable
-fun MapsPreview() {
-    GoogleMapView()
-}
-
-class LocationPermissionWarningViewModel : ViewModel() {
-    private val _dialogActive = MutableStateFlow(false)
-    val dialogActive: StateFlow<Boolean> = _dialogActive.asStateFlow()
-
-    fun triggerDialog() {
-        _dialogActive.value = true
-    }
-
-    fun onDialogAccept() {
-        _dialogActive.value = false
     }
 }
---
+++ app/src/main/java/com/pichugroup/pichuparking/screens/MapViewScreen.kt
@@ -0,0 +1,136 @@
+package com.pichugroup.pichuparking.screens
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.widget.Toast
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.tasks.CancellationTokenSource
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapUiSettings
+import com.google.maps.android.compose.rememberCameraPositionState
+import com.pichugroup.pichuparking.permissions.PermissionAlertDialog
+import com.pichugroup.pichuparking.permissions.RationaleState
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+
+@SuppressLint("MissingPermission")
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun GoogleMapViewScreen() {
+    val context = LocalContext.current
+    val scope = rememberCoroutineScope()
+    val fineLocationPermissionState = rememberMultiplePermissionsState(
+        listOf(
+            Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION,
+        ),
+    )
+    val locationClient = remember {
+        LocationServices.getFusedLocationProviderClient(context)
+    }
+    var rationaleState by remember {
+        mutableStateOf<RationaleState?>(null)
+    }
+    var latLon by remember { mutableStateOf(LatLon(0.0, 0.0)) }
+    Box {
+        DisplayGoogleMaps()
+        rationaleState?.run { PermissionAlertDialog(rationaleState = this) }
+        FloatingActionButton(
+            onClick = {
+                if (fineLocationPermissionState.allPermissionsGranted) {
+                    scope.launch(Dispatchers.IO) {
+                        val priority = Priority.PRIORITY_HIGH_ACCURACY
+                        val result = locationClient.getCurrentLocation(
+                            priority,
+                            CancellationTokenSource().token,
+                        ).await()
+                        result?.let { fetchedLocation ->
+                            latLon = LatLon(
+                                latitude = fetchedLocation.latitude,
+                                longitude = fetchedLocation.longitude
+                            )
+                        }
+                    }
+                } else {
+                    if (fineLocationPermissionState.shouldShowRationale) {
+                        rationaleState = RationaleState(
+                            "Request Precise Location",
+                            "In order to use this feature please grant access by accepting " + "the location permission dialog." + "\n\nWould you like to continue?",
+                        ) { proceed ->
+                            if (proceed) {
+                                fineLocationPermissionState.launchMultiplePermissionRequest()
+                            }
+                            rationaleState = null
+                        }
+                    } else {
+                        fineLocationPermissionState.launchMultiplePermissionRequest()
+                    }
+                    Toast.makeText(context, "Location Permissions not Enabled.", Toast.LENGTH_SHORT).show()
+                }
+            }, modifier = Modifier
+                .padding(16.dp)
+                .align(Alignment.BottomEnd)
+                .clip(CircleShape)
+        ) {
+            Icon(imageVector = Icons.Default.LocationOn, contentDescription = "Current Location")
+        }
+        Text(
+            text = "current Location is: \n Latitude: ${latLon.latitude} \n Longitude: ${latLon.longitude}",
+            modifier = Modifier
+                .padding(16.dp)
+                .align(Alignment.BottomCenter)
+        )
+    }
+}
+
+data class LatLon(
+    val latitude: Double,
+    val longitude: Double,
+)
+
+@Composable
+fun DisplayGoogleMaps() {
+    val singapore = LatLng(1.35, 103.87)
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(singapore, 10f)
+    }
+    val uiSettings by remember { mutableStateOf(MapUiSettings(zoomControlsEnabled = false)) }
+    GoogleMap(
+        modifier = Modifier.fillMaxSize(),
+        cameraPositionState = cameraPositionState,
+        uiSettings = uiSettings,
+    )
+}
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun MapsPreview() {
+    GoogleMapViewScreen()
+}
